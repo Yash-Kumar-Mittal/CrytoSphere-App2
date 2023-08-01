8a37f01d88121449bd38e135ee3360a9db8a94f2
Yash <yashmittal1704@gmail.com>
2023-08-01 22:01:47 +0530

    Deployed crypto app

--- src/app/store.js
+++ src/app/store.js
@@ -8,5 +8,7 @@ export default configureStore({
     reducer: {
         [cryptoApi.reducerPath]: cryptoApi.reducer,
         [cryptoNewsApi.reducerPath]: cryptoNewsApi.reducer,
-    }
+    },
+    middleware: (getDefaultMiddleware) =>
+  getDefaultMiddleware().concat(cryptoApi.middleware, cryptoNewsApi.middleware), 
 })
